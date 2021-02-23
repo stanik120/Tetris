5be1756b2df8ff678e616be1050a6b95ffb40354
Stanik <stanik@tuta.io>
2021-02-23 12:13:33 +0100

    Creating blocks and their rotation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-![Bloks]
+# Simply tetris game written using arcade library
+## types of blocks:
+![Blocks](https://raw.githubusercontent.com/stanik120/Tetris/master/blocks.png?token=AAMAEL2V34GVZLUDDJKZUZDAGTICU)
--- tetris.py
+++ tetris.py
@@ -1,13 +1,76 @@
 """ Simply tetris game """
 import arcade
-
+import random
+# Settings
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 800
+# The size of single square
+BLOCKS_SCALE = 20
+# BLOCKS WIDTH COORDINATE
+I_BLOCK = ((0, 0), (1, 0), (2, 0), (-1,0))
+J_BLOCK = ((0, 0), (1, 0), (-1, 0), (-1,1))
+L_BLOCK = ((0, 0), (1, 0), (-1, 0), (1,1))
+O_BLOCK = ((0, 0), (1, 0), (0, 1), (1,1))
+S_BLOCK = ((0, 0), (-1, 0), (0, 1), (1,1))
+T_BLOCK = ((0, 0), (-1, 0), (1, 0), (0,1))
+Z_BLOCK = ((0, 1), (1, 1), (1, 0), (2,0))
+
+class Block():
+    """ create and hold actual block """
+    def __init__(self):
+        self.x = WINDOW_WIDTH / 2
+        self.y = WINDOW_HEIGHT / 2
+        # randomly selects the type of block
+        random_number = random.randint(0,6)
+        if random_number == 0:
+            self.type_of_block = I_BLOCK
+        elif random_number == 1:
+            self.type_of_block = J_BLOCK
+        elif random_number == 2:
+            self.type_of_block = L_BLOCK
+        elif random_number == 3:
+            self.type_of_block = O_BLOCK
+        elif random_number == 4:
+            self.type_of_block = S_BLOCK
+        elif random_number == 5:
+            self.type_of_block = T_BLOCK
+        elif random_number == 6:
+            self.type_of_block = Z_BLOCK
+
+    def rotate(self):
+        """ rotate the block """
+        new_position = []
+        for elem in self.type_of_block:
+            new_position.append((elem[1], elem[0] * -1))
+
+        self.type_of_block = new_position
+
+    def on_draw(self):
+        """ drow the block """
+        for elem in self.type_of_block:
+            arcade.draw_rectangle_filled(
+                                        center_x= self.x + elem[0] * BLOCKS_SCALE,
+                                        center_y= self.y + elem[1] * BLOCKS_SCALE,
+                                        width= BLOCKS_SCALE,
+                                        height= BLOCKS_SCALE,
+                                        color= arcade.csscolor.WHITE)        
 
 class Tetris(arcade.Window):
     """ main game class """
+    # list of all squares which you have no control
+    squares_list = []
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
+        # crate block
+        self.block = Block()
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.W or symbol == arcade.key.UP:
+            self.block.rotate()
+
+    def on_draw(self):
+        arcade.start_render()
+        self.block.on_draw()
 
 def main():
     """ main function to initialize game """
