7f48b111a4a35ce1a538da939f616ebb6189a4d4
Stanik <stanik@tuta.io>
2021-03-01 23:46:17 +0100

    improved detection when the block is on the floor

--- tetris.py
+++ tetris.py
@@ -6,9 +6,10 @@ WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 800
 # horizontal movement speed (the higher the value, the slower it moves)
 MOVE_SPEED = 4
+FALL_SPEED = 5
 # The size of single square
 BLOCKS_SCALE = 20
-# BLOCKS WIDTH COORDINATE
+# BLOCKS WIDTH COORDINATE (x, y)
 I_BLOCK = ((0, 0), (1, 0), (2, 0), (-1,0))
 J_BLOCK = ((0, 0), (1, 0), (-1, 0), (-1,1))
 L_BLOCK = ((0, 0), (1, 0), (-1, 0), (1,1))
@@ -19,9 +20,15 @@ Z_BLOCK = ((0, 1), (1, 1), (1, 0), (2,0))
 
 class Block():
     """ create and hold actual block """
+    # list of all squares which you have no control
+    squares_list = []
     def __init__(self):
         self.x = WINDOW_WIDTH / 2 + BLOCKS_SCALE / 2
         self.y = WINDOW_HEIGHT / 2 + BLOCKS_SCALE / 2
+        self.can_move_left = True
+        self.can_move_right = True
+        # if square is on ground change to True
+        self.square_on_ground = False
         # randomly selects the type of block
         random_number = random.randint(0,6)
         if random_number == 0:
@@ -43,18 +50,51 @@ class Block():
         """ rotate the block """
         new_position = []
         for elem in self.type_of_block:
-            new_position.append((elem[1], elem[0] * -1))
+            new_position.append((elem[1], -elem[0]))
         # overwrite the positions of the squares in the block with new ones
         self.type_of_block = new_position
 
     def move_left(self):
-        self.x -= BLOCKS_SCALE
+        if self.can_move_left:
+            self.x -= BLOCKS_SCALE
 
     def move_right(self):
-        self.x += BLOCKS_SCALE
+        if self.can_move_right:
+            self.x += BLOCKS_SCALE
+
+    def on_ground(self):
+        """ checks if a square is on the ground, if so moves all squares to square_list """
+        for square in self.type_of_block:
+            # if square is on the bottom off the screen
+            if self.y + square[1] * BLOCKS_SCALE <= BLOCKS_SCALE / 2:
+                self.square_on_ground = True
+                break
+        
+        if self.square_on_ground:
+            # move all square from block to square_list and delet object 
+            for square in self.type_of_block:
+                Block.squares_list.append([self.x + square[0] * BLOCKS_SCALE, self.y + square[1] * BLOCKS_SCALE])
+
+    def update(self):
+        self.on_ground()
+        # block fall down
+        if not self.square_on_ground:
+            self.y -= FALL_SPEED
 
     def on_draw(self):
         """ drow the block """
+        self.can_move_left = True
+        self.can_move_right = True
+        # draw all square where you can't move
+        for square in self.squares_list:
+            arcade.draw_rectangle_filled(
+                                        center_x= square[0],
+                                        center_y= square[1],
+                                        width= BLOCKS_SCALE,
+                                        height= BLOCKS_SCALE,
+                                        color= arcade.csscolor.WHITE)            
+
+        # draw all square in the block where you can move 
         for elem in self.type_of_block:
             arcade.draw_rectangle_filled(
                                         center_x= self.x + elem[0] * BLOCKS_SCALE,
@@ -62,15 +102,19 @@ class Block():
                                         width= BLOCKS_SCALE,
                                         height= BLOCKS_SCALE,
                                         color= arcade.csscolor.WHITE)        
-
+            # disable movement if any block is next to the wall
+            if self.x - BLOCKS_SCALE / 2 + elem[0] * BLOCKS_SCALE == 0:
+                self.can_move_left = False
+            if self.x + BLOCKS_SCALE / 2 + elem[0] * BLOCKS_SCALE == WINDOW_WIDTH:
+                self.can_move_right = False
+            
 class Tetris(arcade.Window):
     """ main game class """
-    # list of all squares which you have no control
-    squares_list = []
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.move_left = False
         self.move_right = False
+        self.move_down = False
         # if it reaches MOVE_SPEED, the move will be made
         self.move_timer = 0
         # crate block
@@ -86,6 +130,9 @@ class Tetris(arcade.Window):
         # move right
         if symbol == arcade.key.D or symbol == arcade.key.RIGHT:
             self.move_right = True
+        # move down
+        if symbol == arcade.key.S or symbol == arcade.key.DOWN:
+            self.move_down = True
 
     def on_key_release(self, symbol: int, modifiers: int):
         # stop moving left
@@ -96,6 +143,9 @@ class Tetris(arcade.Window):
         if symbol == arcade.key.D or symbol == arcade.key.RIGHT:
             self.move_right = False
             self.move_timer = 0
+        # stop moving down
+        if symbol == arcade.key.S or symbol == arcade.key.DOWN:
+            self.move_down = False
 
     def update(self, delta_time: float):
         # reset movement speed counter
@@ -111,6 +161,11 @@ class Tetris(arcade.Window):
             if self.move_timer == 0:
                 self.block.move_right()
             self.move_timer += 1
+
+        self.block.update()
+        # if block is on ground make new object
+        if self.block.square_on_ground:
+            self.block = Block()
 
     def on_draw(self):
         arcade.start_render()
