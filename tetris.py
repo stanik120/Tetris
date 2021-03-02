d44c39587127e7e1bafcfcfb7bb5481f4c0794c8
Stanik <stanik@tuta.io>
2021-03-02 19:50:18 +0100

    Klocki po wypełnieniu lini znikają i zostaje przyznany graczowi punkt, pozostało do zrobienia przesunięcie klocków w dół po dodaniu punktu

--- tetris.py
+++ tetris.py
@@ -2,8 +2,8 @@
 import arcade
 import random
 # Settings
-WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 800
+WINDOW_WIDTH = 200
+WINDOW_HEIGHT = 400
 # horizontal movement speed (the higher the value, the slower it moves)
 MOVE_SPEED = 4
 FALL_SPEED = 5
@@ -19,13 +19,14 @@ T_BLOCK = ((0, 0), (-1, 0), (1, 0), (0,1))
 Z_BLOCK = ((0, 1), (1, 1), (1, 0), (2,0))
 
 def round_y(y):
-    """ round y position off block """
+    """ round y position of block """
     return round(y / BLOCKS_SCALE) * BLOCKS_SCALE
 
 class Block():
     """ create and hold actual block """
     # list of all squares which you have no control
     squares_list = []
+    point = 0
     def __init__(self):
         self.x = WINDOW_WIDTH / 2 + BLOCKS_SCALE / 2
         self.y = WINDOW_HEIGHT
@@ -66,12 +67,32 @@ class Block():
         if self.can_move_right:
             self.x += BLOCKS_SCALE
 
+    def line_check(self):
+        """ checks if any line is full, if so removes squares from it and adds points """
+        square_line_count = {}
+        # create key in square_line_count dictionary for other line and add to them count of all square in this line
+        for square in Block.squares_list:
+            if square[1] in square_line_count:
+                square_line_count[square[1]] += 1
+            else:
+                square_line_count[square[1]] = 1
+        # check count of square in line if is full delet square from square_list and add point and move all other square to the bootom
+        for k, v in square_line_count.items():
+            if v == WINDOW_WIDTH / BLOCKS_SCALE:
+                square_to_remove = []
+                for square in Block.squares_list:
+                    if square[1] == k:
+                        square_to_remove.append(square)
+                for square in square_to_remove:
+                    Block.squares_list.remove(square)
+                Block.point += 1
+
     def on_ground(self):
         """ checks if a square is on the ground, if so moves all squares to square_list """
         for square in self.type_of_block:
             position_x = self.x + square[0] * BLOCKS_SCALE
             position_y = self.y + square[1] * BLOCKS_SCALE
-            # if square is on the bottom off the screen
+            # if square is on the bottom of the screen
             if position_y <= BLOCKS_SCALE:
                 self.square_on_ground = True
                 break
@@ -87,6 +108,7 @@ class Block():
             # move all square from block to square_list and delet object 
             for square in self.type_of_block:
                 Block.squares_list.append([self.x + square[0] * BLOCKS_SCALE, round_y(self.y + square[1] * BLOCKS_SCALE)])
+            self.line_check()
 
     def update(self):
         self.on_ground()
@@ -95,9 +117,16 @@ class Block():
             self.y -= FALL_SPEED
 
     def on_draw(self):
+        """ drow the block """
+        # draw point text
+        arcade.draw_text(
+                        text = str(self.point),
+                        start_x = 10,
+                        start_y = WINDOW_HEIGHT - 25,
+                        color = arcade.csscolor.WHITE,
+                        font_size = BLOCKS_SCALE)
         self.can_move_left = True
         self.can_move_right = True
-        """ drow the block """
         # draw all square where you can't move
         for square in self.squares_list:
             arcade.draw_rectangle_filled(
